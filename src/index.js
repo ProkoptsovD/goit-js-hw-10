255c91c0fe07f63dbd0b10d9cba9b3f2ff68934d
ProkoptsovD <dimka.prokoptsov@gmail.com>
2022-04-05 13:33:43 +0300

    upd

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import fetchCountries from './js/fetchCountries';
-import countryTemplateMarkup from './templates/country-info-tmpl.hbs';
+import createMarkupFromTemplate from './templates/country-info-tmpl.hbs';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 const debounce = require('lodash.debounce');
 
@@ -17,33 +17,55 @@ refs.searchBox.addEventListener('input', debounce(onSearchBoxInput, DEBOUNCE_DEL
 
 function onSearchBoxInput(e) {
   const { value: userQuery } = e.target;
-
-  fetchCountries(userQuery)
-    .then(response => response.json())
-    .then(renderCountryInfo);
-}
-
-function renderCountryInfo(countryArr) {
-  const countriesReturned = getQuantatyOfCountries(countryArr);
-
-  if (countriesReturned.moreThanTen) {
-    alertNotification();
+  if (!userQuery) {
+    destroyRenderedMarkup();
     return;
   }
 
-  refs.countryList.insertAdjacentHTML(
-    'beforeend',
-    countryTemplateMarkup({ countryArr, countriesReturned }),
-  );
+  fetchCountries(userQuery.trim())
+    .then(response => response.json())
+    .then(handleData);
+}
+
+function handleData(data) {
+  try {
+    const countriesReturned = getQuantatyOfCountries(data);
+    const markup = createMarkupFromTemplate({ data, countriesReturned });
+
+    if (countriesReturned.moreThanTen) {
+      Notify.info('To many matches found. Please enter more specific name');
+      return;
+    }
+
+    if (countriesReturned.nothingFound) {
+      Notify.failure('Oops, there is no country with that name');
+      console.clear();
+      return;
+    }
+
+    destroyRenderedMarkup();
+
+    countriesReturned.fromTwoToTen
+      ? inserMarkupTo(refs.countryList, markup)
+      : inserMarkupTo(refs.countryInfo, markup);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 function getQuantatyOfCountries(data) {
   return {
     fromTwoToTen: data.length > 2 && data.length < 10,
     moreThanTen: data.length > 10,
+    nothingFound: data?.status,
   };
 }
 
-function alertNotification() {
-  Notify.info('To many matches found. Please enter more specific name');
+function inserMarkupTo(elem, markup) {
+  elem.insertAdjacentHTML('beforeend', markup);
+}
+
+function destroyRenderedMarkup() {
+  refs.countryList.innerHTML = '';
+  refs.countryInfo.innerHTML = '';
 }
